1b57424a6c8bf0c8874f0c76331611ff1587f48b
Nicolas PINAUD <nicolas.pinaud.ca@gmail.com>
2017-01-08 11:20:16 -0400

    Finalisation installation Realm

---
+++ FollowMyTrip/Location.swift
@@ -0,0 +1,18 @@
+//
+//  Location.swift
+//  FollowMyTrip
+//
+//  Created by Nicolas Pinaud on 08/01/2017.
+//  Copyright © 2017 van. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Location: Object {
+    dynamic var name: String = ""
+    dynamic var latitude: Double = 0
+    dynamic var longitude: Double = 0
+    dynamic var altitude: Double = 0
+    dynamic var comment: String = ""
+}
